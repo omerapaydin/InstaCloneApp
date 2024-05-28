e666f214555487038dd216fd6bd94777cf2e5359
Ömer Apaydın <omerapaydin90@gmail.com>
2024-05-28 17:36:29 +0300

    TableView eklendi

---
+++ InstaCloneApp/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  InstaCloneApp
+//
+//  Created by Ömer on 28.05.2024.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
