869f68ab7b6729448ce55abbbfb67edc151924ea
Ömer Apaydın <omerapaydin90@gmail.com>
2024-06-04 18:04:12 +0300

    Anasayfada veriler gösterildi

--- InstaCloneApp/FeedViewController.swift
+++ InstaCloneApp/FeedViewController.swift
@@ -6,31 +6,88 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController ,UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var userEmailArray = [String]()
+    var userCommentArray = [String]()
+    var likeArray = [Int]()
+    var userImageArray = [String]()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
         
-        
+        getDataFromFirestone()
 
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    
+    func getDataFromFirestone(){
+        
+        let fireStoreDatabase = Firestore.firestore()
+        
+        fireStoreDatabase.collection("Posts").addSnapshotListener { (snapshot, error) in
+            
+            if error != nil {
+                print(error?.localizedDescription)
+            }else {
+                
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    
+                    for document in snapshot!.documents {
+                        let documentID = document.documentID
+                        
+                        if let posteBy = document.get("posteBy") as? String{
+                            self.userEmailArray.append(posteBy)
+                        }
+                        if let postComment = document.get("postComment") as? String{
+                            self.userCommentArray.append(postComment)
+                        }
+                        if let likes = document.get("likes") as? Int{
+                            self.likeArray.append(likes)
+                        }
+                        if let imageUrl = document.get("imageUrl") as? String{
+                            self.userImageArray.append(imageUrl)
+                        }
+                        
+                    }
+                    
+                    self.tableView.reloadData()
+                    
+                    
+                }
+                
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return userEmailArray.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.commentLabel.text = "çok iyi"
+        cell.commentLabel.text = userCommentArray[indexPath.row]
         cell.imageVieww.image = UIImage(named: "select")
-        cell.likeLabel.text = "0"
-        cell.userEmailLabel.text = "omerapaydin"
+        cell.likeLabel.text = String(likeArray[indexPath.row])
+        cell.userEmailLabel.text = userEmailArray[indexPath.row]
         return cell
         
     }
